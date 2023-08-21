4973e605c09857b53efe6f413ec1d6c638b77c06
synodriver <diguohuangjiajinweijun@gmail.com>
2023-08-21 23:02:07 +0800

    prevent from concurrent test

--- tests/testblocklock.py
+++ tests/testblocklock.py
@@ -13,6 +13,9 @@ load_dotenv("./.env")
 
 
 class TestLock(IsolatedAsyncioTestCase):
+    async def delkeys(self):
+        await self.client.delete("RWLOCK:READ", "RWLOCK:WRITE", "RWLOCK:WRITEWAITER")
+
     async def asyncSetUp(self) -> None:
         self.client = Redis(host=os.getenv("REDIS"), max_connections=10)
         self.lock1 = RWLock(self.client)
@@ -30,6 +33,7 @@ class TestLock(IsolatedAsyncioTestCase):
         self.assertFalse(await self.lock1.have_lock("r"))
         print(await self.client.keys("*"))
         self.assertFalse(await self.lock2.have_lock("r"))
+        await self.delkeys()
 
     async def test_read_read(self):
         await self.lock1.acquire("r")
@@ -37,6 +41,7 @@ class TestLock(IsolatedAsyncioTestCase):
         await self.lock1.release("r")
         await self.lock2.release("r")
         print(await self.client.keys("*"))
+        await self.delkeys()
 
     async def test_write_write(self):
         await self.lock1.acquire("w")
@@ -44,6 +49,7 @@ class TestLock(IsolatedAsyncioTestCase):
             await asyncio.wait_for(self.lock2.acquire("w"), 1)
         await self.lock1.release("w")
         print(await self.client.keys("*"))
+        await self.delkeys()
 
     async def test_write_read(self):
         await self.lock1.acquire("w")
@@ -51,6 +57,7 @@ class TestLock(IsolatedAsyncioTestCase):
             await asyncio.wait_for(self.lock2.acquire("r"), 1)
         await self.lock1.release("w")
         print(await self.client.keys("*"))
+        await self.delkeys()
 
     async def test_read_write(self):
         await self.lock1.acquire("r")
@@ -58,6 +65,7 @@ class TestLock(IsolatedAsyncioTestCase):
             await asyncio.wait_for(self.lock2.acquire("w"), 1)
         await self.lock1.release("r")
         print(await self.client.keys("*"))
+        await self.delkeys()
 
     async def test_async_wakeup_read(self):
         """
@@ -79,6 +87,7 @@ class TestLock(IsolatedAsyncioTestCase):
         print(abs(t2 - t1 - 1) < 0.1)
         await self.lock2.release("r")
         print(await self.client.keys("*"))
+        await self.delkeys()
 
     async def test_async_wakeup_write(self):
         """
@@ -100,3 +109,20 @@ class TestLock(IsolatedAsyncioTestCase):
         print(abs(t2 - t1 - 1) < 0.1)
         await self.lock2.release("w")
         print(await self.client.keys("*"))
+        await self.delkeys()
+
+    async def test_write_first(self):
+        async def acquire_task():
+            await self.lock1.acquire("r")
+            await self.lock1.acquire("w")
+
+        asyncio.create_task(acquire_task())
+        await asyncio.sleep(1)
+        self.assertTrue(not await self.client.get("RWLOCK:WRITE"))  # 写锁在等待
+        print(await self.client.keys("*"))
+        print(await self.client.smembers("RWLOCK:READ"))
+        print(await self.client.lrange("RWLOCK:WRITEWAITER", 0, 5))
+        # await self.lock2.acquire("r")
+        with self.assertRaises(asyncio.TimeoutError):  # 此时也不能加读锁了
+            await asyncio.wait_for(self.lock2.acquire("r"), 1)
+        await self.delkeys()
